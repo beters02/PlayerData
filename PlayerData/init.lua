c2f78a7f3c1c9cb9a76784f3d9833ce40146d7f3
Bryce Peters <91118447+beters02@users.noreply.github.com>
2023-10-08 23:55:29 -0600

    Update init.lua

--- PlayerData/init.lua
+++ PlayerData/init.lua
@@ -36,7 +36,7 @@ function PlayerData:GetPath(player, path) -- path: options.primaryFire
     return Strings.convertPathToInstance(path, PlayerData:GetAsync(player))
 end
 
---@summary Set the PlayerData from a new PlayerData
+--@summary Set the PlayerData to a new PlayerData
 type Key = string
 type Path = string
 export type dataChangedInfo = {location: "Set" | "Key" | "Path", key: Key | Path | nil, new: any?}
@@ -60,7 +60,7 @@ function PlayerData:SetKey(player, key, new)
     return PlayerData:Set(player, playerdata, {location = "Key", key = key, new = new})
 end
 
---@summary Set the key of a value from the PlayerData path
+--@summary Set the value of a key from the PlayerData path
 function PlayerData:SetPath(player, path, new)
     local playerdata = PlayerData:GetAsync(player)
     Strings.doActionViaPath(path, playerdata, function(gotTableParent, key)
